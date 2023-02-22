87742499a1df86f439396651dea79e5540e31059
krllrn <74113449+krllrn@users.noreply.github.com>
2023-02-22 11:48:43 +0300

    Commit to actual

--- src/main/java/com/krllrn/viewed/ViewedApplication.java
+++ src/main/java/com/krllrn/viewed/ViewedApplication.java
@@ -1,23 +1,10 @@
 package com.krllrn.viewed;
 
-import org.apache.http.HttpClientConnection;
-import org.apache.http.HttpException;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.protocol.HttpContext;
-import org.apache.http.protocol.HttpRequestExecutor;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import java.io.IOException;
-import java.net.SocketTimeoutException;
 
 @SpringBootApplication
 public class ViewedApplication {
